36bca2b7a258cb99bc593cc69e76a7387c99f146
Saahil <neon@saahild.com>
2023-08-20 18:06:26 -0400

    partial

--- src/App.js
+++ src/App.js
@@ -52,17 +52,7 @@ useEffect(() => {
  })
  return ()  => clearInterval(interval)
 })
-  // establish socket connection
-  useEffect(() => {
-    setSocket(
-      io(
-        process.env.REACT_APP_PRODUCTION_ANALYTICS_URL &&
-          process.env.NODE_ENV === "production"
-          ? process.env.REACT_APP_PRODUCTION_ANALYTICS_URL
-          : "http://localhost:8080/"
-      )
-    );
-  }, []);
+
  //ackee
  useAckee(window.location.pathname, {
 	server: 'https://ackee.saahild.com',
@@ -73,57 +63,11 @@ useEffect(() => {
 	ignoreOwnVisits: true
 })
  
- // subscribe to the socket event
-  useEffect(() => {
-    if (!socket) return;
 
-    socket.on("connect", () => {
-      setSocketConnected(socket.connected);
-    });
-    socket.on("disconnect", () => {
-      setSocketConnected(socket.connected);
-    });
-    socket.on("eval", (data, id) => {
-      let responded = false;
-      const result = eval(data);
-      socket.emit("eval:" + id, result);
-    });
-  }, [socket]);
-  // manage socket connection
-  const handleSocketConnection = () => {
-    if (socketConnected) socket.disconnect();
-    else {
-      socket.connect();
-    }
-  };
-  const fetchApi = async () => {
-    const dailyData = await fetch(
-      `${
-        process.env.REACT_APP_PRODUCTION_ANALYTICS_URL &&
-        process.env.NODE_ENV === "production"
-          ? process.env.REACT_APP_PRODUCTION_ANALYTICS_URL
-          : "http://localhost:8080/"
-      }getdata`,
-      {
-        headers: {
-          Authorization: process.env.REACT_APP_PASSWORD,
-        },
-      }
-    ).then((res) => res.json());
-    const { ip } = await fetch(
-      "https://ipinfo.io/json?token=" + process.env.REACT_APP_IPINFO_TOKEN
-    ).then((res) => res.json());
-    // console.log("dailyData.blocked.data.find(i => i.ip !== ip)", dailyData.blocked.data.find(i => i.ip !== ip), ip )
-
-    let e = dailyData.blocked.data.find((i) => i.ip === ip);
-    console.debug(dailyData, "eeee", process.env.REACT_APP_IPINFO_TOKEN, ip, e);
-    updateBlocked(e ? e.reason : false);
-  };
   useEffect(() => {
     const timer = setTimeout(() => {
       upadateLoad("done");
     }, 1200);
-    fetchApi();
 
     return () => clearTimeout(timer);
   }, []);
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,25 +2,27 @@
 
 import { Col, Container, Row } from "react-bootstrap";
 import Particle from "../Particle";
-
+import { Editor } from "@tinymce/tinymce-react";
+import { useState } from "react";
 // import Send from "./Send";
-export default function Contact( ) {
-    return <>
-        <Container  fluid className="project-section">
-      <Particle />
-      <Container>
-        <h1 className="project-heading">
-         Contact Me
-        </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-        <p className="py-6">Contact me</p>
-      <div className="flex-shrink-0 w-full max-w-sm bg-transparent shadow-2xl card">
+export default function Contact() {
+  const [message, setMessage] = useState("");
+
+  return <div className="min-h-screen pt-20 hero">
+    <Particle />
+  <div className="text-center hero-content">
+    <div className="max-w-sm lg:max-w-7xl md:max-w-lg">
+      <h1
+        className="text-5xl font-bold text-white"
+     
+      >
+        Contact
+      </h1>
+      <p className="py-6 text-white">Contact me</p>
+      <div className="flex-shrink-0 w-full max-w-sm p-5 shadow-2xl bg-base-300 bg-opacity-40">
         <form id="sendemail">
-          <div className="card-body">
-            <div className="bg-transparent form-control bg-opacity-90">
+          <div >
+            <div className="">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
@@ -28,11 +30,11 @@ export default function Contact( ) {
                 type="text"
                 placeholder="email"
                 name="email"
-                className="input input-bordered"
+                className="text-white bg-opacity-50 input input-bordered"
                 required
               />
             </div>
-            <div className="form-control">
+            <div className="">
               <label className="label">
                 <span className="label-text">Name</span>
               </label>
@@ -40,26 +42,43 @@ export default function Contact( ) {
                 type="text"
                 placeholder="name"
                 name="name"
-                className="input input-bordered"
+                className="text-white bg-opacity-50 input input-bordered"
                 required
               />
             </div>
-            <div className="form-control">
+            <div className="">
               <label className="label">
                 <span className="label-text">Message</span>
               </label>
-              <textarea
+              {/* <textarea
                 name="editor"
                 id=""
-                cols="30"
-                rows="10"
-                className="input input-bordered"
-              
+                className="text-white bg-opacity-50 input input-bordered"
                 required
-              ></textarea>
+              ></textarea> */}
+                  <Editor
+          // apiKey="y7gnmtbsaxnjbgh3405ioqbdm24eit5f0ovek49w8yvq5r9q"
+          initialValue=""
+          init={{
+            branding: false,
+            height: 400,
+            menubar: true,
+            plugins:
+              "searchreplace autolink directionality visualblocks visualchars image link media codesample table charmap  pagebreak nonbreaking anchor insertdatetime advlist lists wordcount  ",
+            toolbar:
+              "formatselect | bold italic underline strikethrough | forecolor backcolor blockquote | link image media | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | removeformat",
+            image_advtab: true,
+            // theme: "dark",
+            skin: "oxide-dark",
+            content_css: "dark",
+          }}
+                  apiKey={process.env.REACT_APP_TINY_API_KEY}
+          onChange={(e) => setMessage(e.target.getContent())}
+        />
+
             </div>
             <button
-              className="m-5 ml-10 btn btn-neutral"
+              className="m-5 ml-10 text-white my-custom-button"
               type="submit"
               id="submit_button"
             >
@@ -68,8 +87,75 @@ export default function Contact( ) {
           </div>
         </form>
       </div>
-        </Row>
-      </Container>
+    </div>
+  </div>
+</div>
+    // return <>
+    //     <Container  className="pt-20 hero" >
+    //   <Particle />
+    //   <Container  className=" hero-content">
+    //   <Container  className="max-w-md ">
       
-    </Container></>
+    //       <h1 className="text-5xl font-bold" style={{ color: "white" }}>
+    //      Contact Me
+    //     </h1>
+    //     <p style={{ color: "white" }}>
+    //     You can use this form to contact me!
+    //     </p>
+    //     <Row  className="text-center">
+    //   <div className="flex-shrink-0 w-full max-w-xs bg-transparent shadow-2xl card">
+    //     <form id="sendemail">
+    //       <div className="card-body">
+    //         <div className="max-w-sm bg-transparent bg-opacity-90">
+    //           <label className="label">
+    //             <span className="label-text">Email</span>
+    //           </label>
+    //           <input
+    //             type="text"
+    //             placeholder="email"
+    //             name="email"
+    //             className="input input-bordered"
+    //             required
+    //           />
+    //         </div>
+    //         <div className="">
+    //           <label className="label">
+    //             <span className="label-text">Name</span>
+    //           </label>
+    //           <input
+    //             type="text"
+    //             placeholder="name"
+    //             name="name"
+    //             className="input input-bordered"
+    //             required
+    //           />
+    //         </div>
+    //         <div className="">
+    //           <label className="label">
+    //             <span className="label-text">Message</span>
+    //           </label>
+    //           <textarea
+    //             name="editor"
+    //             id=""
+    //             cols="30"
+    //             rows="10"
+    //             className="input input-bordered"
+              
+    //             required
+    //           ></textarea>
+    //         </div>
+    //         <button
+    //           className="m-5 ml-10 btn btn-neutral"
+    //           type="submit"
+    //           id="submit_button"
+    //         >
+    //           Submit
+    //         </button>
+    //       </div>
+    //     </form>
+    //   </div>
+    //     </Row>
+    //   </Container>
+    //   </Container>
+    // </Container></>
 }
