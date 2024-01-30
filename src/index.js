80dffe0fb43042ba7861c7c9b36bba13a1995820
Saahil <neon@saahild.com>
2024-01-30 18:51:19 -0500

    KITTY

--- src/index.js
+++ src/index.js
@@ -18,6 +18,13 @@ setInterval(() => {
     document.title = "Come Back :("
   }
 }, 200)
+  // oneko import
+  ;(() => {
+  fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
+            .then(x => x.text())
+            .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif"))
+            .then(eval);
+  })()
   const isDev = process.env.NODE_ENV !== "production";
   console.log(
     `%c What's up?`,
